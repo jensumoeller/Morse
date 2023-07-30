7a939a6491a2a11d1dca7ab5daad87e90a1f1827
Jens U. Moeller <jensumoeller@me.com>
2023-07-30 18:22:23 +0200

    Anpassung der Farnsworth-Ermittlung

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Morse
-Python Morse-Code Generator für den Raspberry Pi
+
+Python Morse-Code Generator für den RaspberryPi
+
+Initialer Code wurde über ChatGPT generiert.
+
+Eingabe der Wort- und Fanrsworth Geschwindigkeit in WPM. Eingabe der zu generierenden Wörter.
+
+Vor der Ausführung muss die GPIO Bibliothek installiert werden:
+--> pip install RPi.GPIO
--- morse.py
+++ morse.py
@@ -13,13 +13,14 @@ MORSE_CODE = {
 
 # Definition der Pins des Raspberry Pi
 GPIO.setmode(GPIO.BCM)
-LED_PIN = 18
+LED_PIN = 20
 GPIO.setup(LED_PIN, GPIO.OUT)
 
-# Funktion zum Senden eines Morsezeichens
+# Funktion zum Senden eines Morsezeichens 
+# (Es wird ein einzelnes Zeichen mit Pause dahinter gesendet bzw. Pause bei Leerzeichen)
 def send_morse_character(character, dot_duration):
     if character == ' ':
-        time.sleep(7 * dot_duration)  # Wortpause: 7 Einheiten
+        time.sleep(7 * dot_duration)  # Wortpause: 7 Einheiten (es wurde ein Leerzeichen übergeben)
     else:
         morse_code = MORSE_CODE.get(character.upper())
         if morse_code:
@@ -31,25 +32,29 @@ def send_morse_character(character, dot_duration):
                     GPIO.output(LED_PIN, GPIO.HIGH)
                     time.sleep(3 * dot_duration)
                 GPIO.output(LED_PIN, GPIO.LOW)
-                time.sleep(dot_duration)
-            time.sleep(2 * dot_duration)  # Buchstabenpause: 3 Einheiten
+                time.sleep(dot_duration)  # Pause zwischen Symbolen: 1 Einheit
+            time.sleep(2 * dot_duration)  # Buchstabenpause: 3 Einheiten (2 + 1 aus vorheriger Zeile)
 
 # Funktion zum Senden eines Texts in Morsezeichen
 def send_morse_text(text, dot_duration, farnsworth_delay):
     for character in text:
+        send_morse_character(character, dot_duration)
+        # Zusätzliche Farnsworth Verzögerung nach dem Zeichenende (abhängig von Zeichen oder Leer)
         if character == ' ':
-            time.sleep(3 * dot_duration)  # Wortpause: 3 Einheiten
+            time.sleep(2 * farnsworth_delay)  # Zusätzliche Pause von 2x Farnsworth Delay bei neuem Wort
         else:
-            send_morse_character(character, dot_duration)
-        time.sleep(farnsworth_delay)  # Farnsworth-Verzögerung
+            time.sleep(farnsworth_delay)  # Zusätzliche Pause von 1x Farnsworth-Verzögerung bei neuem Zeichen
 
 # Funktion zum Berechnen der Farnsworth-Verzögerung basierend auf der Zeichengeschwindigkeit (WPM)
-def calculate_farnsworth_delay(dot_duration, words_per_minute):
-    # Einheitliche Morsezeichen-Geschwindigkeit in Sekunden pro Zeichen
-    character_duration = dot_duration * 3
+def calculate_farnsworth_delay(dot_duration, words_per_minute, farnsworth_speed):
 
-    # Farnsworth-Verzögerung berechnen basierend auf der gewünschten WPM-Geschwindigkeit
-    farnsworth_delay = (60.0 / words_per_minute - character_duration) / 2
+    # Berechnung der zusätzlichen Zeit für Pausen, um eine effektive Farnswworth-Geschwindigkeit
+    # zu erreichen (F *  t_dot). In einem Standardwort (PARIS) sind 6 Pausenblocks enthalten,
+    # 4 Pausen zwischen Buchstaben und 2 Pausen (genau 7/3) zwischen Wörtern
+
+    farnsworth_delay_factor = 50 * (words_per_minute / farnsworth_speed - 1)
+    farnsworth_delay = farnsworth_delay_factor * dot_duration / 6
+
     return farnsworth_delay
 
 # Main-Funktion, um den Text einzugeben und die Morsezeichen auszugeben
@@ -57,9 +62,10 @@ def main():
     try:
         text = input("Gib den Text ein, den du als Morsezeichen hören möchtest: ")
         words_per_minute = float(input("Gib die Zeichengeschwindigkeit in WPM (Worte pro Minute) ein: "))
+        farnsworth_speed = float(input("Gib die Farnsworth-Geschwindigkeit in WPM ein: "))
 
         dot_duration = 1.2 / words_per_minute  # Dauer einer Punkt-Einheit in Sekunden
-        farnsworth_delay = calculate_farnsworth_delay(dot_duration, words_per_minute)
+        farnsworth_delay = calculate_farnsworth_delay(dot_duration, words_per_minute, farnsworth_speed)
 
         # Ausgabe des Morsezeichen-Textes auf der Konsole
         print("Morsezeichen:", end=" ")
